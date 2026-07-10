6eddcfd96706f429896e23b014d7a7a457a1c92a
smaarseth-wq <smaarseth@gmail.com>
2026-07-10 21:25:39 +0200

    Update canbus.h

--- src/canbus.h
+++ src/canbus.h
@@ -1,9 +1,37 @@
 #pragma once
 
-#include <stdint.h>
+#include <cstdint>
+#include <string>
 
-bool can_init(const char *iface);
+/*
+ * MaxxECU CAN Simulator
+ * Release_001
+ *
+ * SocketCAN interface for Raspberry Pi
+ */
 
-bool send_rpm(uint16_t rpm);
+class CanBus
+{
+public:
+    CanBus();
+    ~CanBus();
 
-void can_close();
+    // Open CAN interface (typically "can0")
+    bool open(const std::string& interface);
+
+    // Close interface
+    void close();
+
+    // Send one RPM frame
+    bool sendRPM(uint16_t rpm);
+
+    // Returns true if CAN socket is open
+    bool isOpen() const;
+
+private:
+    int socket_fd;
+
+    bool sendFrame(uint32_t id,
+                   const uint8_t* data,
+                   uint8_t dlc);
+};
