017197ffb38fb0f25cdd411f761ebe12ab4052b2
smaarseth-wq <smaarseth@gmail.com>
2026-07-10 21:20:49 +0200

    Improve CAN initialization error handling and sleep duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
+#include "canbus.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
 int main()
 {
-    can_init("can0");
+    if(!can_init("can0"))
+    {
+        std::cout << "CAN init failed\n";
+        return 1;
+    }
+
+    std::cout << "CAN started\n";
+
+    uint16_t rpm = 800;
 
     while(true)
     {
         send_rpm(rpm);
+
+        std::cout << "RPM " << rpm << std::endl;
 
         rpm += 100;
 
         if(rpm > 8000)
             rpm = 800;
 
-        usleep(50000);
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
     }
 }
