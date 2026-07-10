26f6cc4eca95f8603246fb2e8e06a6cd98b8aa33
smaarseth-wq <smaarseth@gmail.com>
2026-07-10 21:18:46 +0200

    Implement main function for CAN initialization and RPM loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,16 @@
+int main()
+{
+    can_init("can0");
 
+    while(true)
+    {
+        send_rpm(rpm);
+
+        rpm += 100;
+
+        if(rpm > 8000)
+            rpm = 800;
+
+        usleep(50000);
+    }
+}
