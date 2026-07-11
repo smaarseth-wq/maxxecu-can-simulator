44fd0a953f9441441744a7571954e5b63ea9e0b4
smaarseth-wq <smaarseth@gmail.com>
2026-07-12 00:28:26 +0200

    Refactor CAN bus functions and improve readability

--- src/canbus.cpp
+++ src/canbus.cpp
@@ -1,7 +1,7 @@
 #include "canbus.h"
 
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 #include <unistd.h>
 #include <sys/socket.h>
@@ -34,9 +34,10 @@ bool CanBus::open(const std::string& interface)
 
     struct ifreq ifr {};
 
-    std::strncpy(ifr.ifr_name,
-                 interface.c_str(),
-                 IFNAMSIZ - 1);
+    std::strncpy(
+        ifr.ifr_name,
+        interface.c_str(),
+        IFNAMSIZ - 1);
 
     if (ioctl(socket_fd, SIOCGIFINDEX, &ifr) < 0)
     {
@@ -53,9 +54,10 @@ bool CanBus::open(const std::string& interface)
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
 
-    if (bind(socket_fd,
-             reinterpret_cast<sockaddr*>(&addr),
-             sizeof(addr)) < 0)
+    if (bind(
+            socket_fd,
+            reinterpret_cast<sockaddr*>(&addr),
+            sizeof(addr)) < 0)
     {
         perror("bind");
 
@@ -65,22 +67,8 @@ bool CanBus::open(const std::string& interface)
         return false;
     }
 
-    std::cout
-        << "--------------------------------------"
-        << std::endl;
-
-    std::cout
-        << "CAN interface : "
-        << interface
-        << std::endl;
-
-    std::cout
-        << "Socket opened successfully."
-        << std::endl;
-
-    std::cout
-        << "--------------------------------------"
-        << std::endl;
+    std::cout << "CAN interface : " << interface << std::endl;
+    std::cout << "Socket opened." << std::endl;
 
     return true;
 }
@@ -99,66 +87,103 @@ bool CanBus::isOpen() const
     return socket_fd >= 0;
 }
 
-bool CanBus::sendFrame(uint32_t id,
-                       const uint8_t* data,
-                       uint8_t dlc)
+bool CanBus::sendFrame(
+    uint32_t id,
+    const uint8_t* data,
+    uint8_t dlc)
 {
     if (!isOpen())
         return false;
 
-    can_frame frame {};
+    struct can_frame frame {};
 
     frame.can_id = id;
     frame.len = dlc;
 
-    std::memcpy(frame.data,
-                data,
-                dlc);
+    std::memcpy(
+        frame.data,
+        data,
+        dlc);
 
-    ssize_t result =
-        send(socket_fd,
-             &frame,
-             sizeof(frame),
-             0);
+    int ret = write(
+        socket_fd,
+        &frame,
+        sizeof(frame));
 
-    if (result != sizeof(frame))
+    if (ret != sizeof(frame))
     {
-        perror("send");
+        perror("write");
         return false;
     }
 
     return true;
 }
 
-bool CanBus::sendRPM(uint16_t rpm)
+bool CanBus::send520(
+    uint16_t rpm,
+    uint16_t tps,
+    uint16_t map,
+    uint16_t lambda)
 {
-    uint8_t data[8] = {0};
-
-    /*
-        MaxxECU test frame
-
-        ID = 0x520
-
-        Byte0 = RPM low
-        Byte1 = RPM high
-    */
+    uint8_t data[8];
 
     data[0] = rpm & 0xFF;
-    data[1] = (rpm >> 8) & 0xFF;
+    data[1] = rpm >> 8;
 
-    bool ok =
-        sendFrame(
-            0x520,
-            data,
-            8);
+    data[2] = tps & 0xFF;
+    data[3] = tps >> 8;
 
-    if (ok)
-    {
-        std::cout
-            << "TX 0x520   RPM = "
-            << rpm
-            << std::endl;
-    }
+    data[4] = map & 0xFF;
+    data[5] = map >> 8;
 
-    return ok;
+    data[6] = lambda & 0xFF;
+    data[7] = lambda >> 8;
+
+    return sendFrame(0x520, data, 8);
+}
+
+bool CanBus::send521(
+    int16_t coolant,
+    int16_t airtemp,
+    uint16_t battery,
+    uint16_t oilpressure)
+{
+    uint8_t data[8];
+
+    data[0] = coolant & 0xFF;
+    data[1] = coolant >> 8;
+
+    data[2] = airtemp & 0xFF;
+    data[3] = airtemp >> 8;
+
+    data[4] = battery & 0xFF;
+    data[5] = battery >> 8;
+
+    data[6] = oilpressure & 0xFF;
+    data[7] = oilpressure >> 8;
+
+    return sendFrame(0x521, data, 8);
+}
+
+bool CanBus::send522(
+    uint16_t speed,
+    int16_t ignition,
+    uint16_t fuelDuty,
+    uint16_t ethanol)
+{
+    uint8_t data[8];
+
+    data[0] = speed & 0xFF;
+    data[1] = speed >> 8;
+
+    data[2] = ignition & 0xFF;
+    data[3] = ignition >> 8;
+
+    data[4] = fuelDuty & 0xFF;
+    data[5] = fuelDuty >> 8;
+
+    data[6] = ethanol & 0xFF;
+    data[7] = ethanol >> 8;
+
+    return sendFrame(0x522, data, 8);
 }
